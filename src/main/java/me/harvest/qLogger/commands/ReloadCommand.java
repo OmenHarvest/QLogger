2e977e4f79e2a9c0d8c29c0b6d1420e2d618e892
Dmitry <omenharvest@gmail.com>
2024-11-08 04:13:04 +0300

    fixed qlogger command

--- src/main/java/me/harvest/qLogger/commands/ReloadCommand.java
+++ src/main/java/me/harvest/qLogger/commands/ReloadCommand.java
@@ -10,7 +10,12 @@ import org.jetbrains.annotations.NotNull;
 public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if(strings[0].equalsIgnoreCase("reload")) {
+
+        if(strings.length < 1 && (commandSender.hasPermission("QLogger.admin") || commandSender.isOp())) {
+            TextUtils.startupWelcome();
+            return true;
+        }
+        if(strings[0].equals("reload")) {
             if (commandSender.hasPermission("QLogger.admin") || commandSender.isOp()) {
                 QLogger.getInstance().reloadConfig();
                 TextUtils.MessageSend(commandSender, "[QLogger]", "config reloaded", 87, 95, 207);
